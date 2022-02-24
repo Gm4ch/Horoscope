0223f526ade8bcc7f8ed5da09a9ec02f42ab3501
Gm4ch <90480288+Gm4ch@users.noreply.github.com>
2022-02-23 23:26:43 -0600

    Update Horoscope.py

--- Horoscope.py
+++ Horoscope.py
@@ -1,5 +1,6 @@
 import calendar
 import datetime
+from enum import Enum
 #importing the calender library
 
 
@@ -10,7 +11,32 @@ def num_to_month(monthNum):
     month = (months[monthNum])
     return month #returning the month name when the function is called
 
-
+def month_to_num(monthName):
+    if monthName == "january":
+        month_Num = 1
+    if monthName == "febuary":
+        month_Num = 2
+    if monthName == "march":
+        month_Num = 3
+    if monthName == "april":
+        month_Num = 4
+    if monthName == "may":
+        month_Num = 5
+    if monthName == "june":
+        month_Num = 6
+    if monthName == "july":
+        month_Num = 7
+    if monthName == "august":
+        month_Num = 8
+    if monthName == "september":
+        month_Num = 9
+    if monthName == "october":
+        month_Num = 10
+    if monthName == "november":
+        month_Num = 11
+    if monthName == "december":
+        month_Num = 12
+    return int(month_Num)
 
 def chineseZodiacSign(sign_num):
     sign_text = ["Monkey","Rooster","Dog","Pig","Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Sheep"]
@@ -25,7 +51,7 @@ def chinese_zodiac_calc(year):
 invalidMonth = True 
 while (invalidMonth):  #while the month is invalid the loop runs
     try:
-        birthMonth = input("What month were you born on? ").lower().strip() #asking user when they were born
+        birthMonth = (input("What month were you born on? ").lower().strip()) #asking user when they were born
         if(birthMonth.capitalize() in list(calendar.month_name)): #if the month that the user entered is a valid month in the calender library the loop ends
             birthMonth = birthMonth.lower()
             invalidMonth = False
@@ -34,29 +60,38 @@ while (invalidMonth):  #while the month is invalid the loop runs
             birthMonth = birthMonth.lstrip("0")
             int(birthMonth)
             invalidMonth = False
-            num_to_month(birthMonth) #the program calls on the function to convert the number into a name
         else:
             print("Not a valid month name. check for misspelling or incorrect number format") #if neither of the arguments are true the program tells the user thier input is wrong and keeps looping.
     except ValueError:
         print("Not a valid month name. check for misspelling or incorrect number format") #if neither of the arguments are true the program tells the user thier input is wrong and keeps looping.
 
+try: 
+    int(birthMonth)
+    stringMonth = num_to_month(birthMonth)
+except ValueError:
+    stringMonth = num_to_month(birthMonth)
 
 invalidDay = True
 while (invalidDay):
     try: #telling the computer to try the following
-        birthDay = input("what day of " + num_to_month(birthMonth) + " were you born on? ")
+        birthDay = int(input("what day of " + stringMonth + " were you born on? "))
         if int(birthDay) in range(1,32):
             invalidDay = False
         else:
             print("please provide a valid answer")
     except ValueError: #if the computer has a "Value error" error type rather than the program shutting down it does the following
         print("please provide a valid answer")
-    
+''' MAIN AREA OF PROBLEM
+if type(birthMonth) == str:
+    intMonth = month_to_num(birthMonth)
+elif type(birthMonth) == int or type(birthMonth) == float:
+    intMonth = birthMonth
+'''
 invalidYear = True
 while (invalidYear):
     try: #telling the computer to try the following
         birthYear = input("what year were you born on? ")
-        if datetime.datetime(int(birthYear), int(birthMonth), int(birthDay)) and len(birthYear) == 4: #This line breaks the program when you input a month name instead of a month number. have to change the month name back to a number
+        if datetime.datetime(int(birthYear), intMonth, int(birthDay)) and len(birthYear) == 4:
             invalidYear = False
         else:
             print("please provide a valid answer")
